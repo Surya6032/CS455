a4ddaa37811153b8f303c745147d297259b6bed1
Surya Singh <psurya.924@gmail.com>
2020-01-27 23:59:36 -0600

    Update style.php

--- style.php
+++ style.php
@@ -1,13 +1,56 @@
+<!DOCTYPE HTML>  
 <html>
-<body>
+<head>
+<style> 
+input[type=button], input[type=submit], input[type=reset] {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-decoration: none;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+textarea {
+  width: 50%;
+  height: 150px;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: #f8f8f8;
+  resize: none;
+}
+input[type=text] {
+  width: 50%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: 3px solid #ccc;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+  outline: none;
+}
+
+input[type=text]:focus {
+  border: 3px solid #555;
+}
+</style>
+</head>
+<body>  
+
 <?php
-$name = $email = $message= "";
+// define variables and set to empty values
+$name = $email = $message = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = test_input($_POST["name"]);
   $email = test_input($_POST["email"]);
   $message = test_input($_POST["message"]);
-  
+ 
 }
+
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
@@ -15,10 +58,33 @@ function test_input($data) {
   return $data;
 }
 ?>
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?><br>
-Your message is:<?php echo $_POST["message"];?><br><br>
 
+<h2>Contact Us</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Message: <textarea name="message" rows="5" cols="40"></textarea>
+  <br><br>
+ 
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+
+<?php
+echo "<h2>Your Input:</h2>";
+?>
+Name:
+<?php
+echo $name;?><br>
+Email:
+<?php
+echo $email;
+?><br>
+Message:
+<?php echo $message;
+?><br>
 
 
 </body>

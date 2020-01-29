576c2f20df9f88d643050a7086fc19d15fe44b32
Surya Singh <psurya.924@gmail.com>
2020-01-29 17:49:29 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "cs455";
+
+// Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT id, ftname, fname FROM students";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo "id: " . $row["id"] . " - Name: " . $row["fname"] ;
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?>
+    </form>
+</body>
+</html>

74b10c72dba3524324eed5701143b30519194f85
Surya Singh <psurya.924@gmail.com>
2020-02-05 17:20:55 -0600

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Login:
+        <form action="welcome.php" method="get">
+            Username: <input type="text" name="username">
+            Password: <input type="password" name="pwd">
+            Remember Me: <input type="checkbox" name="remember" value="ON" />
+            <input type="submit">
+        </form>
+        
+    </body>
+</html>

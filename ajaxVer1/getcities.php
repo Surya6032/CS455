70621b47ffb0ddb3247675c3b1fc2e446960164c
Surya Singh <psurya.924@gmail.com>
2020-03-04 16:28:59 -0600

    Add files via upload

---
+++ ajaxVer1/getcities.php
@@ -0,0 +1,18 @@
+<?php
+
+  //print_r($_REQUEST);
+  if ($_GET['state'] == 'Colorado'){
+	echo "<select>";
+	echo "<option value='Denver'>Denver</option>";
+	echo "</select>";
+  }else{
+	  
+	echo "<select>";
+	echo "<option value='Florence'>Florence</option>";
+	echo "<option value='Tuscumbia'>Tuscumbia</option>";
+	echo "<option value='Muscle Shoals'>Muscle Shoals</option>";
+	echo "</select>";
+	  
+	  
+	  
+  }
